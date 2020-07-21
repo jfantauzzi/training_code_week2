8bb986878da46e40c89fd0aac35f6aeeb18772ec
jacob fantauzzi <jfantauzzi90@gmail.com>
2020-07-21 11:03:40 -0600

    Initial Commit

---
+++ PizzaStore/PizzaStore.Domain/Models/Order.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace PizzaStore.Domain.Models
+{
+  public class Order
+  {
+    public List<Pizza> Pizzas { get; set; }
+    public DateTime DateOrdered { get; set; }
+
+    public void CreatePizza()
+    {
+      Pizzas.Add(new Pizza());
+    }
+
+  }
+}
---
+++ PizzaStore/PizzaStore.Domain/Models/Pizza.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace PizzaStore.Domain.Models
+{
+  public class Pizza
+  {
+    public List<Topping> Toppings { get; set; }
+    public Crust Crust { get; set; }
+    public Size Size { get; set; }
+  }
+}
