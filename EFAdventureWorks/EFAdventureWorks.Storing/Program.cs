dbf7a4adde90157fc9eff5ddea63bef61f0ad22e
jacob fantauzzi <jfantauzzi90@gmail.com>
2020-07-24 15:25:44 -0600

    Finished up sql content for this week. Added database with EF

---
+++ EFAdventureWorks/EFAdventureWorks.Storing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFAdventureWorks.Storing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
